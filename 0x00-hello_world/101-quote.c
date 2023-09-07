7df9d54d9b8c11719167bda13e29827b17845924
yahya bnoun <yahyaiko22@gmail.com>
2023-09-07 14:55:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
 #include <string.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
+	char mes = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, mes, strlen(mes));
 	return (1);
 }
 
