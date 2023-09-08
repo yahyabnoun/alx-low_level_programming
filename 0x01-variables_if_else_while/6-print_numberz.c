b70e4b6f24fc6003c646dd282cd9fc95231a1de0
yahya bnoun <yahyaiko22@gmail.com>
2023-09-08 16:48:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
