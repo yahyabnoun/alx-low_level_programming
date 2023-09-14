1eeffc1aa656c3951e1af652b8aa3b893e2c5c06
yahyabnoun <yahyaiko22@gmail.com>
2023-09-14 15:31:27 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * print_diagonal - draws a diagonal line
- * @n: The number of times the \ should be printed.
+ * print_square - prints a square of x-height and x-width (basically a square)
+ * with hashtags.
+ * @size: size of the square that should be printed
  *
  * Return: Nothing (always)
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i;
 	int j;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < size; j++)
 			{
-				_putchar(' ');
+				_putchar('#');
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
