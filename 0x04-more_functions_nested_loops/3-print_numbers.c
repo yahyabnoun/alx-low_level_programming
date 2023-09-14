36040bf67b4bb0157a9a06a6fe3f0d1f476b1fe2
yahyabnoun <yahyaiko22@gmail.com>
2023-09-14 14:33:06 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: Nothing (always)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
