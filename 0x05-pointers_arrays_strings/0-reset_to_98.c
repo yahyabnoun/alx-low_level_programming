5d39b5f26e7d5c3fa76fd70ba04ce38a2ce901e0
yahyabnoun <yahyaiko22@gmail.com>
2023-09-18 16:29:25 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 /**
- * reset_to_98 - reset_to_98.
- * @c: Integer
- * void function
- */
+* reset_to_98 - reset_to_98.
+* @c: Integer
+* void function
+*/
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
