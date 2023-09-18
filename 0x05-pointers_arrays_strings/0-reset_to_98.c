9196cd5df401ad806b70e019b621e4286450d3df
yahyabnoun <yahyaiko22@gmail.com>
2023-09-18 16:02:35 +0100

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - reset_to_98.
+ * @c: Integer
+ *
+ * Return: 1 if it's a digit
+ * 0 if not
+ */
+void reset_to_98(int *n)
+{
+    *n=98;
+    printf("n=%d\n", *n);
+}
