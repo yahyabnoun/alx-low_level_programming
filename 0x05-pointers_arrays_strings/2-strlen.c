2f5b1f8b17878b59f93fcc11b08feb2ccd442650
yahyabnoun <yahyaiko22@gmail.com>
2023-09-18 22:28:18 +0100

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,14 @@
-#include "main.h"
 /**
- * _strlen - lenght of s
- * @s: string
- *
- * Return: nothing.
- */
+* _strlen - lenght of s
+* @s: array
+* Return: int.
+*/
 int _strlen(char *s)
 {
-   int len = 0;
-   while(s[len]!='\0')
-   {
-        len++;
-   }
-   return len;
+int len = 0;
+while(s[len]!='\0')
+{
+len++;
+}
+return len;
 }
