816216e6924d22ad19eb8567ef4e413893157283
yahyabnoun <yahyaiko22@gmail.com>
2023-09-22 23:43:21 +0100

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,12 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+
+
+
+
+
 
 #endif
