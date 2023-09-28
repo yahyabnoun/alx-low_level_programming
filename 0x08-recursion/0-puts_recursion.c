adebd350355b07ade22f674ad7fe71f4d25a65d1
yahyabnoun <yahyaiko22@gmail.com>
2023-09-28 18:20:20 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - _puts_recursion
+ * @s: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+    if(*s=='\0')
+    {
+        _putchar('\n');
+        return;
+    }
+    _putchar(*s);
+    _puts_recursion(++s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+#endif
