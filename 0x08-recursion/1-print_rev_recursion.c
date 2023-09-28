394fd858b692eb385e30ecd54371735729407c2f
yahyabnoun <yahyaiko22@gmail.com>
2023-09-28 20:01:31 +0100

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _print_rev_recursion - _print_rev_recursion
+ * @s: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+ }
