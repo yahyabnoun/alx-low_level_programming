0acffb05021d3ffb2486b8aa58fb45b1f4a8160e
yahyabnoun <yahyaiko22@gmail.com>
2023-10-02 22:06:44 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+if (argc>=3)
+printf("%d\n", (atoi(argv[1])*atoi(argv[2])));
+else
+printf("%s\n","Error");
+return (0);
+}
