8cffddf60ad9230cd949d225472b1dcbbc8dafa0
yahyabnoun <yahyaiko22@gmail.com>
2023-10-02 18:22:28 +0100

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+    int i;
+for (i=0;i < argc;i++)
+printf("%s\n", argv[i]);
+return (0);
+}
