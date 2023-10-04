03eaf8ae877fa33e5b201afa26ed94c0468c09e8
yahyabnoun <yahyaiko22@gmail.com>
2023-10-04 22:40:22 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,10 @@ char *str_concat(char *s1, char *s2)
 char *t;
 int i, size;
 
-if (s1 == NULL)
-{
-return (NULL);
-}
-size = strlen(s1) ;
 
-t = (char *)malloc((sizeof(char) * size) +1);
+size = strlen(s1);
+
+t = (char *)malloc((sizeof(char) * size));
 
 if (t == NULL)
 {
@@ -32,8 +29,8 @@ for (i = 0; s1[i] != '\0'; i++)
 t[i] = s1[i];
 
 for (i = 0; s2[i] != '\0'; i++)
-t[size+i] = s2[i];
+t[size + i] = s2[i];
 }
-t[size+i]= '\0';
+t[size + i] = '\0';
 return (t);
 }
