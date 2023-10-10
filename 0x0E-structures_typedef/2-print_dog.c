c7dd3a98f8a5f89403ba6dfeaf48c994f9642809
yahyabnoun <yahyaiko22@gmail.com>
2023-10-10 23:15:48 +0100

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,12 +3,17 @@
 /**
  * print_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
+ * Return: void
  */
 
 void print_dog(struct dog *d)
 {
 if (d)
 {
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,  d->age,  d->owner);
 }
 }
