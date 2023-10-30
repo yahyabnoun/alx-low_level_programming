c524707469999ad71099da9d1d7d589c8a34f490
yahya bnoun <yahyaiko22@gmail.com>
2023-10-30 23:19:33 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, w = 0;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
