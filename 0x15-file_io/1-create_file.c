5537cbe702fcdf243e5ad704e8109ed8ae82e7e6
yahya bnoun <yahyaiko22@gmail.com>
2023-10-30 23:33:37 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,5 @@
 #include "main.h"
-
-/**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
+#include <string.h>
 
 /**
  * create_file - Creates a file.
@@ -26,14 +11,16 @@ size_t _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int o, w = 0;
-
-	if (filename == NULL || text_content == NULL)
+    int len = strlen(text_content);
+	if (!filename)
 		return (-1);
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, _strlen(text_content));
+	if (o == -1)
+		return (-1);
 
-	if (o == -1 || w == -1)
+	w = write(o, text_content, len);
+	if (w == -1)
 		return (-1);
 
 	close(o);
