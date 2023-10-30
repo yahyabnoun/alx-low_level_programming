c687f30c0bded90cebc39146d89a26231d38ed6e
yahya bnoun <yahyaiko22@gmail.com>
2023-10-30 23:26:24 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,29 @@
+ * @o: variabale , if file not opne return -1
 #include "main.h"
-#include <string.h>
 
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
+ *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w = 0;
+	int o, w, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, strlen(text_content));
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
