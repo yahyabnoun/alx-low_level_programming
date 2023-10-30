a3ff7b881f1ce9d6f3f85599307f157db2c45d55
yahya bnoun <yahyaiko22@gmail.com>
2023-10-30 23:24:52 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,8 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- *
+ * @o: variabale , if file not opne return -1
+ * @w: variabale , if file not write return -1
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
